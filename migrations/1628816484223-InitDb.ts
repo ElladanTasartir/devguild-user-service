b2b9f1876401d51d8e8362bc4a9f1e500a86353d
Erick Malta <erickmalta100@gmail.com>
2021-08-12 22:20:16 -0300

    chore(migrations): add InitDb migration

---
+++ migrations/1628816484223-InitDb.ts
@@ -0,0 +1,41 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class InitDb1628816484223 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      'CREATE TABLE users (id varchar(36) PRIMARY KEY NOT NULL, username varchar NOT NULL, email varchar NOT NULL, created_at timestamptz NOT NULL, updated_at timestamptz NOT NULL)',
+    );
+
+    await queryRunner.query(
+      'CREATE TABLE user_technologies (technology_id int NOT NULL, user_id vachar(36) NOT NULL)',
+    );
+
+    await queryRunner.query(
+      'CREATE TABLE project_members (user_id varchar(36) NOT NULL, project_id varchar(36) NOT NULL)',
+    );
+
+    await queryRunner.query(
+      'CREATE TABLE user_comments (id varchar(36) PRIMARY KEY NOT NULL, user_id varchar(36), project_id varchar(36) NOT NULL, created_at timestamptz NOT NULL, updated_at timestamptz NOT NULL)',
+    );
+
+    await queryRunner.query(
+      'ALTER TABLE user_comments ADD CONSTRAINT FK_user_comments_user FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE SET NULL UPDATE CASCADE',
+    );
+
+    await queryRunner.query(
+      'ALTER TABLE project_members ADD CONSTRAINT FK_project_members_user FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE UPDATE CASCADE',
+    );
+
+    await queryRunner.query(
+      'ALTER TABLE user_technologies ADD CONSTRAINT FK_user_technologies_user FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE ON UPDATE CASCADE',
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      'ALTER TABLE user_technologies DROP CONSTRAINT FK_user_technologies_user',
+    );
+
+    await queryRunner.query('');
+  }
+}
