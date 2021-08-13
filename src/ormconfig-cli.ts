eff588dbdac02a70dca9fe90d4f9332057d1c8c7
Erick Malta <erickmalta100@gmail.com>
2021-08-13 20:29:14 -0300

    feat(ormconfig): add entities to ormconfig

--- src/ormconfig-cli.ts
+++ src/ormconfig-cli.ts
@@ -1,9 +1,10 @@
 import { User } from './modules/user/entities/user.entity';
+import { Technology } from './modules/user/entities/user-technologies.entity';
 import { ormConfig } from './ormconfig';
 
 export default {
   ...ormConfig,
-  entities: [User],
+  entities: [User, Technology],
   migrations: ['migrations/*.ts'],
   cli: {
     migrationsDir: 'migrations',
--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -1,12 +1,14 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { postgres } from './config';
+import { Technology } from './modules/user/entities/user-technologies.entity';
+import { User } from './modules/user/entities/user.entity';
 
 const { host, username, password, database, port, logging, synchronize } =
   postgres;
 
 export const ormConfig = {
   type: 'postgres',
-  autoLoadEntities: true,
+  entities: [User, Technology],
   host,
   port,
   username,
