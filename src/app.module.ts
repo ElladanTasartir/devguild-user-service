2d7961f1a96477beb8c526a1b330c75eaf36ba35
Erick Malta <erickmalta100@gmail.com>
2021-08-14 21:45:37 -0300

    feat(app): add project module to app

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { UserModule } from './modules/user/user.module';
+import { ProjectModule } from './modules/project/project.module';
 import { ormConfig } from './ormconfig';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(ormConfig), UserModule],
+  imports: [TypeOrmModule.forRoot(ormConfig), UserModule, ProjectModule],
   controllers: [AppController],
 })
 export class AppModule {}
