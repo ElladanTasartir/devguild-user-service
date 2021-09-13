6dad8c5425e599e7fc0aa0d4c42434e39836be03
Erick Malta <erickmalta100@gmail.com>
2021-09-12 23:56:08 -0300

    feat(main): add loggin

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import './config';
 import { port } from './config';
 
 async function bootstrap() {
+  const logger = new Logger('Main');
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(port);
+  await app.listen(port, () => {
+    logger.verbose(`Application running on port ${port}`);
+  });
 }
 bootstrap();
