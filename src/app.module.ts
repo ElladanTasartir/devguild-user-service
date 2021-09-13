bfb3fef0628928eb57d7efd4dd7a88d3410a9e5c
Erick Malta <erickmalta100@gmail.com>
2021-09-12 23:54:03 -0300

    feat(app): connect to mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,19 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { UserModule } from './modules/user/user.module';
 import { ProjectModule } from './modules/project/project.module';
 import { ormConfig } from './ormconfig';
+import { mongodb } from './config';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(ormConfig), UserModule, ProjectModule],
+  imports: [
+    TypeOrmModule.forRoot(ormConfig),
+    MongooseModule.forRoot(mongodb.uri),
+    UserModule,
+    ProjectModule,
+  ],
   controllers: [AppController],
 })
 export class AppModule {}
